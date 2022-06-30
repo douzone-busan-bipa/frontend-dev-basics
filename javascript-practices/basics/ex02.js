ffe1512a96a7cbdcd568ab378c9216b7ad5b1b00
douzone-busan-bipa <douzone.busan.bipa@gmail.com>
2022-06-30 14:19:32 +0900

    basic - ex02 #2

--- javascript-practices/basics/ex02.js
+++ javascript-practices/basics/ex02.js
@@ -8,10 +8,14 @@
     - boolean
     [객체]
     - object
+        1.
+        2. {} 객체 리터럴, (J)ava(S)cript (O)bject (N)otation
+        3. []
+        4. null
     - function
 */
 
-var u = undefined;
+var u = undefined; // var u; 동일, 선언과 정의의 구분이 없다.
 var i = 10;
 var s = "Hello World";
 var b = true;
@@ -26,9 +30,24 @@ console.log("===객체1[object type]==============");
 var i2 = new Number(10);
 var s2 = new String("Hello World");
 var b2 = new Boolean(true);
+var o = new Object();
+var a = new Array();
+var d = new Date();
+
+var o2 = {};
+var a2 = [];
+var n = null;
+
 console.log("i2:" + typeof(i2));
 console.log("s2:" + typeof(s2));
 console.log("b2:" + typeof(b2));
+console.log("o:" + typeof(o));
+console.log("o2:" + typeof(o2));
+console.log("a:" + typeof(a));
+console.log("a2:" + typeof(a));
+console.log("d:" + typeof(d));
+console.log("n:" + typeof(n));
+
 
 
 
@@ -44,4 +63,7 @@ console.log("f:" + typeof(f));
 
 
 // 원시 타입에 메소드가 호출 될 때....
-console.log(b.valueOf());
+// 임시 Wrapper 객체가 만들어져서 메소드 호출이 일어난 뒤 사라진다: 유사 객체
+console.log("===원시 타입도 메소드 호출이 가능하다?[function type]==============");
+console.log(b.valueOf());   // new Boolean(b).valueOf()
+console.log(b2.valueOf());
